21b2a9796c0fb1966881cb99ede471f0136190bb
ArjanBaweja <127305034+ArjanBaweja@users.noreply.github.com>
2026-05-16 16:01:45 -0400

    blah

--- TeamCode/src/main/java/Subsystems/Drive/SwerveModules.java
+++ TeamCode/src/main/java/Subsystems/Drive/SwerveModules.java
@@ -1,7 +1,10 @@
 package Subsystems.Drive;
 
+import static androidx.core.math.MathUtils.clamp;
+
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -17,16 +20,53 @@ public class SwerveModules {
 
     private static final double absError = 0.02; //acceptable angle error
 
-    public SwerveModules(DcMotorEx motor, CRServo servo, AnalogInput encoder, HardwareMap hm){
+    public SwerveModules(DcMotorEx motor, CRServo servo, AnalogInput encoder){
         driveMotor = motor;
         steerServo = servo;
         aE = encoder;
-
+        driveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void applyPower(double P){
-
         driveMotor.setPower(P);
+    }
+
+    public double getCurrentAngle() {
+        return Math.atan2(Math.sin((aE.getVoltage() / 3.3) * 2 * Math.PI), Math.cos((aE.getVoltage() / 3.3) * 2 * Math.PI)); //angle [pi,-pi]
+    }
+
+    public void Update(double targetAngle, double power) {
+        double current = getCurrentAngle();
+        double error = Math.atan2(Math.sin(targetAngle - current), Math.cos(targetAngle - current)); //error [pi,-pi]
+
+        //optimization shuold be better in case of like at 10 target -100
+        if (Math.abs(error) > Math.PI / 2) {
+            targetAngle = targetAngle + Math.PI;
+            power       = -power;
+            error       = Math.atan2(Math.sin(targetAngle - current), Math.cos(targetAngle - current)); //error [pi,-pi]
+        }
+
+        if (Math.abs(error) < absError) {
+            steerServo.setPower(0);
+            driveMotor.setPower(power);
+            lastError = 0;
+            return;
+        }
+
+        // pppppppppppppppppppppppppppppppppppppppdddddddddddddddddddddddddddddddddddddddddddddddd
+        double P = kP * error;
+        double D = kD * (error - lastError);
+        lastError = error;
+
+        steerServo.setPower(clamp(P + D, -1.0, 1.0));
+        driveMotor.setPower(power);
+    }
+
+    public void stop() {
+        steerServo.setPower(0);
+        driveMotor.setPower(0);
+        lastError = 0;
     }
 
 
--- TeamCode/src/main/java/Teleops/ModulesTest.java
+++ TeamCode/src/main/java/Teleops/ModulesTest.java
@@ -1,4 +1,44 @@
 package Teleops;
 
-public class ModulesTest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import Subsystems.Drive.SwerveModules;
+
+
+@TeleOp(name="SingleModuleTest")
+public class ModulesTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+        SwerveModules module = new SwerveModules(
+                hardwareMap.get(DcMotorEx.class,  "fld"),
+                hardwareMap.get(CRServo.class,    "fls"),
+                hardwareMap.get(AnalogInput.class,"fle")
+        );
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double x = gamepad1.left_stick_x;
+            double y = -gamepad1.left_stick_y;
+
+            double angle = Math.atan2(x, y);
+            double power = Math.hypot(x, y);
+
+            if (power > 0.05) {
+                module.Update(angle, power);
+            } else {
+                module.stop();
+            }
+
+            telemetry.addData("target", Math.toDegrees(angle));
+            telemetry.addData("postion", Math.toDegrees(module.getCurrentAngle()));
+            telemetry.addData("power", power);
+            telemetry.update();
+        }
+    }
 }
